b88a235bd9f6bcf5450a7363fce46f062922df5d
software <engsoftoware@gmail.com>
2026-09-09 21:58:26 +0300

    feat: add course review entity

---
+++ CourseManagement.Domain/Entities/CourseReview.cs
@@ -0,0 +1,17 @@
+namespace CourseManagement.Domain.Entities;
+
+public class CourseReview
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+    public User User { get; set; } = null!;
+
+    public int CourseId { get; set; }
+    public Course Course { get; set; } = null!;
+
+    public int Rating { get; set; }
+    public string Comment { get; set; } = string.Empty;
+    public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAtUtc { get; set; } = DateTime.UtcNow;
+}
